3e85b81e10c288d33f48a7fd12acdbeef336a154
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-03-28 17:02:57 +0700

    feat: add namespaces to your URLconf

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+from django.db.models import F
 from .models import Question, Choice
 
 
@@ -17,6 +19,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -34,6 +42,6 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
